763a3a37ed1781f848fcb43cd373fd77614502ee
alejandrogsk <alejandrogsk9900@gmail.com>
2023-04-07 14:02:56 -0300

    Dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-
+import { ThemeProvider } from "next-themes"
+import DarkTogglerBtn from "~/components/DarkTogglerBtn";
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps}  />
+      <DarkTogglerBtn />
+    </ThemeProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
---
+++ src/pages/auth.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const auth = () => {
+  return (
+    <main className='grid grid-cols-1 md:grid-cols-2'>
+        <div>
+            <form>
+                <h1 className='dark:text-red text-blue'>Login</h1>
+
+            </form>
+        </div>
+        <div>
+            register
+        </div>
+    </main>
+  )
+}
+
+export default auth
